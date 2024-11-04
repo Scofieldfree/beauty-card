f5260be63df398ac828ff899758cf1f6c339a03a
scofield <muduhssgf@163.com>
2024-11-04 21:39:24 +0800

    update:add demo text

--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -7,8 +7,8 @@ const Tiptap = () => {
   const editor = useEditor({
     extensions: [StarterKit],
     content: `<p>👋 hi 你好
-🎨 Memo Card 可以帮你生成精美的文字卡片，可应用于你日常书摘、笔记、备忘等内容的分享。
-💡 你可以在这里输入文字尝试一下，支持 Markdown 语法，实时生效。</p>`,
+🎨 Beauty Card 可以帮你生成精美的文字卡片，可应用于你日常书摘、笔记、备忘等内容的分享。
+💡 你可以在这里输入文字尝试一下。</p>`,
   })
 
   return <EditorContent editor={editor} />
