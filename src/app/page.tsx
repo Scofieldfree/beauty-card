85814e416a8f1c24184585800f03dea007b104d0
scofield <muduhssgf@163.com>
2024-11-04 20:52:01 +0800

    update:add page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,16 @@
 import React, { useRef } from "react";
 import { toPng } from "html-to-image";
 
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import Tiptap from "@/components/Tiptap";
 
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
-type CardProps = React.ComponentProps<typeof Card>;
 
-export default function Home({ className, ...props }: CardProps) {
-  const imageRef = useRef(null);
-  console.log({ imageRef });
-  const htmlToImageConvert = () => {
+
+export default function Home() {
+  const imageRef = useRef();
+    const htmlToImageConvert = () => {
     toPng(imageRef.current, { cacheBust: false })
       .then((dataUrl) => {
         const link = document.createElement("a");
@@ -27,12 +25,11 @@ export default function Home({ className, ...props }: CardProps) {
         console.log(err);
       });
   };
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="p-[30px] bg-[#C8F4FC] rounded-[8px]" ref={imageRef}>
-          <Card className={cn("w-[380px] rounded-[16px]", className)} {...props}>
+          <Card className="w-[380px] rounded-[16px]">
             <CardHeader>
               <CardTitle>
                 <div contenteditable>beauty card</div>
