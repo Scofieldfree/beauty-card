6d2e7bbf5ab51dcad10c1335698fe184a1ff1975
scofield <muduhssgf@163.com>
2024-11-04 21:29:23 +0800

    update:add page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,12 @@
-"use client";
+import BeautyCard from "@/components/BeautyCard"
 
-// import Image from "next/image";
-import React, { useRef } from "react";
-import { toPng } from "html-to-image";
-
-import { Button } from "@/components/ui/button";
-import Tiptap from "@/components/Tiptap";
-
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
-  const imageRef = useRef();
-  const htmlToImageConvert = () => {
-    toPng(imageRef?.current, { cacheBust: false })
-      .then((dataUrl) => {
-        const link = document.createElement("a");
-        link.download = "beauty-card.png";
-        link.href = dataUrl;
-        link.click();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="p-[30px] bg-[#C8F4FC] rounded-[8px]" ref={imageRef}>
-          <Card className="w-[380px] rounded-[16px]">
-            <CardHeader>
-              <CardTitle>
-                <div contenteditable>beauty card</div>
-              </CardTitle>
-              <CardDescription>You have 3 unread messages.</CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-4">
-              <Tiptap className="h-[100px" />
-            </CardContent>
-            <CardFooter>
-              <Button className="w-full">Mark all as read</Button>
-            </CardFooter>
-          </Card>
-        </div>
-        <div className="w-[200px]">
-          <Button onClick={htmlToImageConvert}>Export Image</Button>
-        </div>
+      <BeautyCard />
       </main>
     </div>
   );
---
+++ src/components/BeautyCard.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React, { useRef, RefObject } from "react";
+import { toPng } from "html-to-image";
+
+import { Button } from "@/components/ui/button";
+import Tiptap from "@/components/Tiptap";
+
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function BeautyCard() {
+  const imageRef = useRef<HTMLDivElement| null>(null);
+  async function htmlToImageConvert(imageRef: RefObject<HTMLDivElement | null>) {
+    const dataUrl = await toPng(imageRef.current, { cacheBust: false })
+    const link = document.createElement("a");
+    link.download = "beauty-card.png";
+    link.href = dataUrl;
+    link.click();
+
+      // .then((dataUrl) => {
+      //   const link = document.createElement("a");
+      //   link.download = "beauty-card.png";
+      //   link.href = dataUrl;
+      //   link.click();
+      // })
+      // .catch((err) => {
+      //   console.log(err);
+      // });
+  };
+
+  return (
+    <div>
+      <div className="p-[30px] bg-[#C8F4FC] rounded-[8px]" ref={imageRef}>
+        <Card className="w-[380px] rounded-[16px]">
+          <CardHeader>
+            <CardTitle>
+              <div>beauty card</div>
+            </CardTitle>
+            <CardDescription>You have 3 unread messages.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <Tiptap />
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Mark all as read</Button>
+          </CardFooter>
+        </Card>
+      </div>
+      <div className="w-[200px] pt-[10px]">
+        <Button onClick={() => htmlToImageConvert(imageRef)}>Export Image</Button>
+      </div>
+    </div>
+  );
+}
